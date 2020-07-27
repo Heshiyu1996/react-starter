ce27c9515175d33a4ff7c60a50d43adfb7d38ae0
heshiyu <heshiyu@corp.netease.com>
2020-07-27 16:40:31 +0800

    feat: danmu demo

---
+++ src/components/danmu/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    text: 'text1',
+    start: 0,
+    id: 10000,
+  },
+  {
+    text: 'text2',
+    start: 1000,
+    id: 10001,
+  },
+  {
+    text: 'text3',
+    start: 1000,
+    id: 10002,
+  },
+];
+
+export default data;
